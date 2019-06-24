871f8bcd9dd15b312d8dcae515ad6a78712c5648
Kelvin Frade <kelvinfrade@gmail.com>
2019-06-24 16:59:00 -0300

    termino da aplicação

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
+//import { Link } from 'react-router-dom';
 
 import './Feed.css';
 
@@ -9,46 +11,64 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
-    state = {
-        feed: [],
-    }; 
-    async componentDidMount(){
-        const response = await api.get('posts');
+  state = {
+    feed: [],
+  };
+  async componentDidMount() {
+    const response = await api.get('posts');
 
-        this.setState ({feed: response.data });
+    this.setState({ feed: response.data });
+  }
 
-    }
+  registerToSocket = () => {
+    const socket = io('https://instalab.herokuapp.com');
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post => (post.id === likedPost._id ? likedPost : post)),
+      });
+    });
+  };
+
+  handleLike = id => {
+    api.post(`/posts/${id}/like`);
+  };
 
   render() {
     return (
       <section id="post-list">
-          { this.state.feed.map(post => (
-                <article>
-                <header>
-                  <div className="user-info">
-                    <span>{post.author}</span>
-                    <span className="place">{post.place}</span>
-                  </div>
-                  <img src={more} alt="Mais" />
-                </header>
-                <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+        {this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
+              <img src={more} alt="Mais" />
+            </header>
 
-                <!--img src="https://avatars1.githubusercontent.com/u/28929274?s=280&v=4" alt="" /--> 
-      
-                <footer>
-                  <div className="actions">
-                    <img src={like} alt="" />
-                    <img src={comment} alt="" />
-                    <img src={send} alt="" />
-                  </div>
-                  <strong>{post.likes} curtidas</strong>
-                  <p>
-                    {post.descriptions}
-                    <span>{post.hashtags}</span>
-                  </p>
-                </footer>
-              </article>
-          ))}
+            <img src={`https://instalab.herokuapp.com/files/${post.image}`} alt="" />
+
+            <footer>
+              <div className="actions">
+                <button type="button" onClick={() => this.handleLike(post._id)}>
+                  <img src={like} alt="" />
+                </button>
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
+              <strong>{post.likes} curtidas</strong>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ import New from './pages/New';
 
 function Routes() {
   return (
-      <Switch>
-          <Route path="/" exact component={Feed} />
-          <Route path="/new" component={New} />
-        </Switch>
+    <Switch>
+      <Route path="/" exact component={Feed} />
+      <Route path="/new" component={New} />
+    </Switch>
   );
 }
 
